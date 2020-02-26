063db543752458aeaa58bbfd0de49732661b9397
seba <seba.g90@gmail.com>
2020-02-26 22:19:58 +0100

    resolved bug if query vector is empty

--- bag_of_words/text_sim.py
+++ bag_of_words/text_sim.py
@@ -2,13 +2,14 @@ from nltk.stem.snowball import SnowballStemmer
 import os
 import math
 import operator
+import copy
 
 
 # retrieve articles
 def retrieve_articles():
     strings = {}
-    for filename in os.listdir('./bestand'):
-        path = "./bestand/" + filename
+    for filename in os.listdir('./testbestand'):
+        path = "./testbestand/" + filename
         with open(path, "r") as file:
             my_string = file.read()
             strings[filename] = my_string
@@ -138,28 +139,32 @@ def main():
         if user_input == "...":
             break
         # create a local copy of the cleaned and stemmed dict
-        articles = articles_org.copy()
+        articles = copy.deepcopy(articles_org)
 
         # clean and stem user input
         cleaned_input = clean_string(user_input)
         stemmed_input = str_2_vec(cleaned_input, stopwords)
-        articles["query"] = stemmed_input
+       
+        if len(stemmed_input) != 0:
+            articles["query"] = stemmed_input
+            
+            # create document matrix terms
+            matrix_terms = create_matrix_terms(articles)
 
-        # create document matrix terms
-        matrix_terms = create_matrix_terms(articles)
+            # calculate frequency list for every term 
+            freq_list = calc_freq(articles, matrix_terms)
+            for key in articles:
+                vec = calculate_vec(matrix_terms, articles[key])
+                articles[key] = vec
 
-        # calculate frequency list for every term 
-        freq_list = calc_freq(articles, matrix_terms)
-        for key in articles:
-            vec = calculate_vec(matrix_terms, articles[key])
-            articles[key] = vec
+            # calculate TF * IFD (1 - log(n/nj))
+            articles = calc_tf_idf(articles, freq_list)   
 
-        # calculate TF * IFD (1 - log(n/nj))
-        articles = calc_tf_idf(articles, freq_list)   
-
-        # calculate vector cos similarity and print out the best result
-        results = find_best_match(articles)
-        print(max(results.items(), key=operator.itemgetter(1))[0])
+            # calculate vector cos similarity and print out the best result
+            results = find_best_match(articles)
+            print(max(results.items(), key=operator.itemgetter(1))[0])
+        else:
+            print("Sorry, try with another query")
     
 
 if __name__ == "__main__":
