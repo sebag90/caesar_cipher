9f878ee16537e93af615ac39bbab1b18de57c0da
seba <seba.g90@gmail.com>
2020-02-26 22:02:42 +0100

    comments + corrected TDxIFD

--- bag_of_words/text_sim.py
+++ bag_of_words/text_sim.py
@@ -34,8 +34,8 @@ def clean_string(stringa):
     return bella
 
 
+# collect stop words after removing whitespaces and /n
 def collect_stopwords():
-    # collect stop words after removing whitespaces and /n
     nonowords = []
     with open("stopwords.txt") as stopwords:
         for word in stopwords:
@@ -44,7 +44,7 @@ def collect_stopwords():
             nonowords.append(newword2)
     return nonowords
 
-
+# remove stopwords and stem a string
 def str_2_vec(input_string, nonowords):
     # extract single words
     splits = input_string.split()
@@ -60,7 +60,7 @@ def str_2_vec(input_string, nonowords):
         stemmed.append(stemmer.stem(element))
     return stemmed
 
-
+# create matrix term list
 def create_matrix_terms(_articles):
     matrix_terms = []
     for key in _articles:
@@ -69,7 +69,7 @@ def create_matrix_terms(_articles):
                 matrix_terms.append(term)
     return matrix_terms
         
-
+# calculate TF vector for every stemmed document
 def calculate_vec(matrix_terms, stemmed_list):
     string_vec = []
     for single_term in matrix_terms:
@@ -80,7 +80,7 @@ def calculate_vec(matrix_terms, stemmed_list):
         string_vec.append(counter)
     return string_vec
 
-
+# calculate the number of document in which every term is present
 def calc_freq(_articles, matrix_terms):
     counters = []
     for term in matrix_terms:
@@ -91,15 +91,15 @@ def calc_freq(_articles, matrix_terms):
         counters.append(counter)
     return counters
 
-
+# calculate TF * IDF (1 + log(n/nj))
 def calc_tf_idf (_articles, freq_list):
     n = len(_articles)
     for key in _articles:
         for i in range(len(freq_list)):
-            _articles[key][i] = _articles[key][i] * math.log(n/freq_list[i])
+            _articles[key][i] = _articles[key][i] * (1 + math.log(n/freq_list[i]))
     return _articles
             
-
+# calculate cos similarity
 def find_best_match(_articles):
     results = {}
     for key in _articles:
@@ -111,38 +111,54 @@ def find_best_match(_articles):
                 pprod = pprod + _articles[key][i] * _articles["query"][i] 
                 ln = ln + ((_articles[key][i])**2)
                 lq = lq + ((_articles["query"][i])**2)
-            cos = pprod / (1 + math.sqrt(ln * lq))
+            cos = pprod / math.sqrt(ln * lq)
             results[key] = cos
     return results
             
             
 def main():
-    articles = retrieve_articles()
-    stopwords = collect_stopwords()
     # TODO
-    # 1-process first all documents then add query in while loop 
-    # 2-add support for ENG/ITA
-    articles["query"] =  input("Enter data query:\n> ")
+    # add support for ENG/ITA
     
-    for element in articles:
-        new_str = clean_string(articles[element])
-        articles[element] = new_str
-     
-    for key in articles:
-        articles[key] = str_2_vec(articles[key], stopwords)
+    # retrieve all articles from folder bestand and stopwords
+    articles_org = retrieve_articles()
+    stopwords = collect_stopwords()
+    # clean the articles
+    for element in articles_org:
+        new_str = clean_string(articles_org[element])
+        articles_org[element] = new_str
+    # stem the articles
+    for key in articles_org:
+        articles_org[key] = str_2_vec(articles_org[key], stopwords)
     
-    matrix_terms = create_matrix_terms(articles)
 
-    freq_list = calc_freq(articles, matrix_terms)
-     
-    for key in articles:
-        vec = calculate_vec(matrix_terms, articles[key])
-        articles[key] = vec
+    while True:
+        user_input =  input("Enter data query or enter '...' to exit:\n> ")
+        if user_input == "...":
+            break
+        # create a local copy of the cleaned and stemmed dict
+        articles = articles_org.copy()
 
-    articles = calc_tf_idf(articles, freq_list)   
+        # clean and stem user input
+        cleaned_input = clean_string(user_input)
+        stemmed_input = str_2_vec(cleaned_input, stopwords)
+        articles["query"] = stemmed_input
 
-    results = find_best_match(articles)
-    print(max(results.items(), key=operator.itemgetter(1))[0])
+        # create document matrix terms
+        matrix_terms = create_matrix_terms(articles)
+
+        # calculate frequency list for every term 
+        freq_list = calc_freq(articles, matrix_terms)
+        for key in articles:
+            vec = calculate_vec(matrix_terms, articles[key])
+            articles[key] = vec
+
+        # calculate TF * IFD (1 - log(n/nj))
+        articles = calc_tf_idf(articles, freq_list)   
+
+        # calculate vector cos similarity and print out the best result
+        results = find_best_match(articles)
+        print(max(results.items(), key=operator.itemgetter(1))[0])
     
 
 if __name__ == "__main__":
