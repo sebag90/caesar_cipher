45d399e640f1182cb0e26e5d9177efcea6d31621
seba <seba@localhost.localdomain>
2020-03-07 23:55:32 +0100

    cipher working

---
+++ caesar_cipher/cipher.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+std::vector <char> create_alphabet(std::vector <char> v, int k){
+    std::vector <char> b;
+    for (int i=v.size() - k; i <= v.size() - 1; i++){
+        b.push_back(v[i]);
+    }
+
+    for (int j = 0; j < v.size() - k; j++){
+        b.push_back(v[j]);
+    }
+    return b;
+}
+
+
+std::string cipher(std::string input_s, std::vector <char> alphb_real, std::vector <char> alphb_cip){
+    std::string message;
+    for (int i=0; i<input_s.size(); i++){
+        if (ispunct(input_s[i]) || isspace(input_s[i])){
+            message.push_back(input_s[i]);
+        }
+        for (int j=0; j<alphb_real.size(); j++){
+            if (input_s[i] == alphb_cip[j]){
+                message.push_back(alphb_real[j]);
+            }
+        }
+    }
+    return message;
+}
+
+
+int main(){
+    std::vector <char> a {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int key;
+    while (true){
+        std::string input_string;
+        std::cout << "Please enter a message to cipher or '...' to exit\n> ";
+        
+        std::getline(std::cin, input_string);
+        if (input_string == "..."){
+            break;
+        }
+        std::cout << "Please enter cipher key (1 - 26)\n> ";
+        std::cin >> key;
+        // flush the newline character out of the buffer between cin and getline in next loop
+        std::cin.ignore();
+        if (key > 0 && key < 26){
+            std::vector <char> cipher_alphabet = create_alphabet(a, key);
+            std::string output = cipher(input_string, a, cipher_alphabet);
+            std::cout << output << "\n\n";
+        }
+        else{
+            std::cout << "sorry, try that again\n";
+        }
+    }
+}
