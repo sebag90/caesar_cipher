8d934200d747c3a4c23f9fbfa3860e5e9ac9c531
seba <seba.g90@gmail.com>
2020-04-04 10:46:49 +0200

    read_file boost test

--- tests/read_file/main.cpp
+++ tests/read_file/main.cpp
@@ -1,16 +1,32 @@
-#include <iostream>
+#define BOOST_TEST_MODULE boost_test_macro_overview
+#include <boost/test/included/unit_test.hpp>
+
 #include <vector>
+#include <algorithm>    
 #include <string>
 #include "../../functions.hpp"
 
 
-
-int main(){
+BOOST_AUTO_TEST_CASE( test_macro_overview )
+{
+    std::vector <std::string> expected {"this is a test",
+                                        "this is another test", 
+                                        "khaleesi is marrying jack sparrow"};
+    
+    std::vector <std::string> results;
     std::vector <std::string> file_names;
     file_names = retrieve_files();
 
     for (auto x : file_names){
-        std::string output = read_file(x);
-        std::cout << output << std::endl;
+        std::string message = read_file(x);
+        results.push_back(message);           
     }
+
+    std::sort (expected.begin(), expected.end());
+    std::sort (results.begin(), results.end());
+
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(results.begin(), results.end(), 
+                              expected.begin(), expected.end());
+  
 }
