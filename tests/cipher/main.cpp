0be744f557c6001962f4741370442a2ad3df0017
seba <seba.g90@gmail.com>
2020-04-17 11:13:41 +0200

    test cipher

--- tests/cipher/main.cpp
+++ tests/cipher/main.cpp
@@ -1,17 +1,29 @@
-#include <iostream>
+#define BOOST_TEST_MODULE boost_test_macro_overview
+#include <boost/test/included/unit_test.hpp>
 #include <string>
 #include "../../functions.hpp"
 
 
+namespace tt = boost::test_tools;
 
-int main(){
-    
+BOOST_AUTO_TEST_CASE( test_no_1){
+
     std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
     int key = 4;
-
     std::string cipher_alphabet = create_alphabet(alphabet, key);
     std::string message = "ciao";
 
-    std::cout << cipher(message, alphabet, cipher_alphabet) << std::endl;
+    BOOST_TEST(cipher(message, alphabet, cipher_alphabet) == "gmes"); 
+}
 
+BOOST_AUTO_TEST_CASE( test_no_2){
+
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    int key = 7;
+    std::string cipher_alphabet = create_alphabet(alphabet, key);
+    std::string message = "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
+
+    std::string result = "Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.";
+
+    BOOST_TEST(cipher(message, alphabet, cipher_alphabet) == result); 
 }
